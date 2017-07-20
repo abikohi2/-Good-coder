5fbb7ea140a2a946e96ccd74c106deca361af62c
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-20 18:23:27 +0530

    Rename num.c to number.c

---
+++ number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num==0)
+{
+printf("the num is zero");
+}
+elseif(num>0)
+{
+printf("the num is positive");
+}
+else
+{
+printf("the num is negative");
+}
+}
