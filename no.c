afb12cc77ba4807e3716ca5bfae76561e138f6c9
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-21 16:32:38 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("Enter the Number");
+scanf("%d",&number);
+if(number==0)
+printf("Number is Zero");
+elseif(number>0)
+printf("positive");
+else
+printf("Negative");
+}
