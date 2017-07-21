e66d84a8ba908ad19440442c025b4950c3ab9101
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-21 17:25:52 +0530

    Create evenno.C

---
+++ evenno.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,n;
+printf("Enter the Number");
+scanf("%d",&num);
+n=num%2;
+if(n==0)
+{
+printf("Number is even");
+}
+else
+{
+printf("Number is odd");
+}
+}
