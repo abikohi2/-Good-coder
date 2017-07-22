b25018ea53894a5c7d7210bd94cb7a5b80e9b7ec
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-22 17:55:28 +0530

    Create no.C

---
+++ no.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int no, i, sum = 0;
+    
+    printf("Enter a positive integer ");
+    scanf("%d",&no);
+
+    for(i=1; i <= no; ++i)
+    {
+        sum += i;   
+    }
+
+    printf("Sum = %d",sum);
+}
