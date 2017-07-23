6615517cdda8b323259254a8dfa4d9834bede62a
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-23 15:11:22 +0530

    Create nodd.C

---
+++ nodd.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int 
+printf("enter the m value");
+scanf("%d",&m);
+printf("enter the n value");
+scanf("%d",&n);
+for(i=m;i<=n;i++)
+{
+f=i%2;
+if(f!=0)
+{
+printf("odd number is %d",f)
+}
+}
+
