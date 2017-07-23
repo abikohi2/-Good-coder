f604b0ad165a8dfdfd30ce0663030eb562649f82
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-23 11:52:02 +0530

    Create power.C

---
+++ power.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int 
+printf("enter the number as base");
+scanf("%d",&n);
+printf("enter the power value");
+scanf("%d",&m);
+for(i=1;i<=m,i++)
+{
+m=m*n;
+}
+printf("power value is %d",&m);
+}
