94ea21e8f04c194900171aa420a47f9cd9a0c93c
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-24 10:07:39 +0530

    Create amg.C

---
+++ amg.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,s,rem;
+printf("enter the number");
+scanf("%d",&n);
+while()
+{
+rem=n%10;
+s=s+rem*rem*rem;
+n=n/10;
+}
+if(s==n)
+printf("number is armstrong");
+else
+printf("not armstrong");
+}
