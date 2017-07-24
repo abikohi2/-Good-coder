386e6ddb6232e99add8ef1892c1a25228d2b4b53
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-07-24 16:21:29 +0530

    Create xt.C

---
+++ xt.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int j;
+char Q;
+printf("Print 1to3d again and again");
+while(1){
+for(j=1;j<=5;j++)
+printf("\n%d",j);
+ch=getch();
+if(ch=='Q')
+exit(0);
+}
+getch();
+}
