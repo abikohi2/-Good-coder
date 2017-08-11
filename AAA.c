f7c6d5ec3089f45f714c5b5fc75a6745161a766f
abikohi2 <30317770+abikohi2@users.noreply.github.com>
2017-08-11 16:38:05 +0530

    Create AAA.c

---
+++ AAA.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int
+printf("Enter the number");
+scanf("%d,%d".&a,&b);
+while(a!=b)
+{
+if(a<b)
+{
+res=a-b;
+}
+else
+{
+res=b-a;
+}
+}
+printf("the LCM is %d",res);
+}
+
